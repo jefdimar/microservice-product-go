b0a812fe99492f9dc4ef8d9f468eb780a0e676f2
telkomdev-jefri <jefdimar@gmail.com>
2024-10-28 13:32:33 +0700

    adding route health

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,15 +1,21 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
-
 	"go-microservice-product-porto/app/business"
 	"go-microservice-product-porto/app/controllers"
 	"go-microservice-product-porto/app/repositories"
 	"go-microservice-product-porto/config"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
+	// Default route
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome to the Product Service"})
+	})
+
 	// Initialize repository
 	productRepo := repositories.NewProductRepository(config.DB)
 
