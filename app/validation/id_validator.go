693f550bce7cda83a674942482ec7ba7df26e984
telkomdev-jefri <jefdimar@gmail.com>
2024-11-14 21:59:01 +0700

    adding validation for empty id

--- app/validation/id_validator.go
+++ app/validation/id_validator.go
@@ -7,6 +7,10 @@ import (
 )
 
 func ValidateObjectID(id string) error {
+	if id == "" {
+		return fmt.Errorf("id is required")
+	}
+
 	if !primitive.IsValidObjectID(id) {
 		return fmt.Errorf("invalid id format")
 	}
