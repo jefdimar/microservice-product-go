03822f7f739207af9ce595739df595da4181131c
telkomdev-jefri <jefdimar@gmail.com>
2025-02-18 21:54:37 +0700

    changing the package of helpers into common

--- pkg/common/date_formatter.go
+++ pkg/common/date_formatter.go
@@ -1,7 +1,18 @@
-package helpers
+package common
 
 import "time"
 
+// FormatDateTime formats a time.Time object into a string in the format "DD-Mon-YYYY HH:mm:ss"
 func FormatDateTime(t time.Time) string {
 	return t.Format("02-Jan-2006 15:04:05")
+}
+
+// FormatDate formats time to DD-Mon-YYYY
+func FormatDate(t time.Time) string {
+	return t.Format("02-Jan-2006")
+}
+
+// FormatTime formats time to HH:mm:ss
+func FormatTime(t time.Time) string {
+	return t.Format("15:04:05")
 }
